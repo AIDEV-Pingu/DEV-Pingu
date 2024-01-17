309aa7d17e6206c9d12db750e1271c077d3de8ea
ahyun39 <70644095+ahyun39@users.noreply.github.com>
2024-01-17 18:41:44 +0900

    pingu-front

---
+++ pingu_front/src/SsgPage.js
@@ -0,0 +1,13 @@
+// SsgPage.js
+import React from 'react';
+
+const SsgPage = () => {
+  return (
+    <div>
+      <h2>SSG 페이지</h2>
+      {/* SSG 페이지 내용 */}
+    </div>
+  );
+}
+
+export default SsgPage;
