07fedbeed447c0b4c3419e6fa18e8cb529c8c90c
QhyunQ <liscawol@gmail.com>
2024-01-18 17:42:24 +0900

    1/18 기초 메인페이지 구현

--- pingu/imgback/views.py
+++ pingu/imgback/views.py
@@ -12,6 +12,8 @@ from .models import ImageUpload
 
 # Create your views here.
 nlp = spacy.load('content/Pingu_model_1_17_1000')
+nlp2 = spacy.load('content/Pingu_model_1_17_500')
+nlp3 = spacy.load('content/Pingu_model_1_15')
 
 def image_upload_view(request):
     if request.method == 'POST':
@@ -33,7 +35,7 @@ def ocr_view(request, image_id):
     model = project.version(settings.ROBOFLOW_VERSION).model
 
     # 이미지에 OCR 적용
-    org_img, cropped_img = predict2crop(model, image_path)
+    cropped_img = predict2crop(model, image_path)
 
     if cropped_img is None:
         print("No valid image to process")
@@ -51,7 +53,20 @@ def ocr_view(request, image_id):
         elif ent.label_ == "가격":
             price = ent.text
 
+    '''
+    case = 0
+    while product_name is None or price is None or case < 5:
+        case += 1
+        doc = nlp(ocr_result)
+        for ent in doc.ents:
+            if ent.label_ == "상품명" and product_name is None:
+                product_name = ent.text
+            elif ent.label_ == "가격" and price is None:
+                price = ent.text
+    '''
+
     return render(request, 'imgback/ocr_result.html', {
+        'ocr_result' : ocr_result,
         'image_path': processed_img,
         'product_name': product_name,
         'price': price
