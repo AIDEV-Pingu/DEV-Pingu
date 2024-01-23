45d805b7f4109d2e683f031f2a1d1d53e806ba3f
ahyun39 <70644095+ahyun39@users.noreply.github.com>
2024-01-23 15:18:32 +0900

    pingu-front update

---
+++ pingu_front/src/CoupangPage.js
@@ -0,0 +1,13 @@
+// CoupangPage.js
+import React from 'react';
+
+const CoupangPage = () => {
+  return (
+    <div>
+      <h2>Coupang 페이지</h2>
+      {/* Coupang 페이지 내용 */}
+    </div>
+  );
+}
+
+export default CoupangPage;
