f4bc0eeb6bdc006ff582446d9bb88b1338ed64f2
choigapju <chgju35@gmail.com>
2024-01-23 14:42:45 +0900

    01.23.crwal

--- pingu-front/src/MusinsaPage.js
+++ pingu-front/src/MusinsaPage.js
@@ -1,43 +1,29 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-const MusinsaPage = () => {
-  const [data, setData] = useState(null);  // 크롤링 데이터를 저장할 state
+function MusinsaPage() {
+    const [data, setData] = useState([]);
 
-  useEffect(() => {
-    // 백엔드에서 크롤링한 데이터를 가져오는 함수
-    const fetchData = async () => {
-      try {
-        const response = await fetch('http://your-django-backend-url/api/crawling/musinsa');
-        const data = await response.json();
-        setData(data);  // 크롤링 데이터를 state에 저장
-      } catch (error) {
-        console.error('There has been a problem with your fetch operation:', error);
-      }
-    };
+    useEffect(() => {
+        axios.get('/crawling_results_view?site=musinsa&product_name=your_product_name')
+            .then(response => {
+                setData(response.data.musinsa);
+            });
+    }, []);
 
-    fetchData();
-  }, []);
-
-  return (
-    <div>
-      <h2>무신사 페이지</h2>
-      {/* 크롤링 데이터를 화면에 표시 */}
-      {data ? (
+    return (
         <div>
-          {/* 데이터 형식에 따라 적절하게 수정하세요 */}
-          {data.map((item, index) => (
-            <div key={index}>
-              <h3>{item['Product_Name']}</h3>
-              <p>{item['Price']}</p>
-              <a href={item['Product_Link']}>상품 링크</a>
-            </div>
-          ))}
+            <h2>무신사페이지</h2>
+            {data.length > 0 ? data.map((item, index) => (
+                <div key={index}>
+                    <h2>{item.Product_Name}</h2>
+                    <p>{item.Price}</p>
+                    <a href={item.Product_Link}>Link</a>
+                </div>
+            )) : <h2>검색된 결과가 없습니다</h2>}
+            <br/><br/>
         </div>
-      ) : (
-        <p>Loading...</p>
-      )}
-    </div>
-  );
-};
+    );
+}
 
 export default MusinsaPage;
--- pingu-front/src/SsgPage.js
+++ pingu-front/src/SsgPage.js
@@ -1,43 +1,29 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-const SsgPage = () => {
-  const [data, setData] = useState(null);  // 크롤링 데이터를 저장할 state
+function SsgPage() {
+    const [data, setData] = useState([]);
 
-  useEffect(() => {
-    // 백엔드에서 크롤링한 데이터를 가져오는 함수
-    const fetchData = async () => {
-      try {
-        const response = await fetch('http://your-django-backend-url/api/crawling/ssg');
-        const data = await response.json();
-        setData(data);  // 크롤링 데이터를 state에 저장
-      } catch (error) {
-        console.error('There has been a problem with your fetch operation:', error);
-      }
-    };
+    useEffect(() => {
+        axios.get('/crawling_results_view?site=ssg&product_name=your_product_name')
+            .then(response => {
+                setData(response.data.ssg);
+            });
+    }, []);
 
-    fetchData();
-  }, []);
-
-  return (
-    <div>
-      <h2>SSG 페이지</h2>
-      {/* 크롤링 데이터를 화면에 표시 */}
-      {data ? (
+    return (
         <div>
-          {/* 데이터 형식에 따라 적절하게 수정하세요 */}
-          {data.map((item, index) => (
-            <div key={index}>
-              <h3>{item['Product Name']}</h3>
-              <p>{item['Price']}</p>
-              <a href={item['Product URL']}>상품 링크</a>
-            </div>
-          ))}
+            <h2>SSG페이지</h2>
+            {data.length > 0 ? data.map((item, index) => (
+                <div key={index}>
+                    <h2>{item.product_names}</h2>
+                    <p>{item.prices}</p>
+                    <a href={item.product_urls}>Link</a>
+                </div>
+            )) : <h2>검색된 결과가 없습니다</h2>}
+            <br/><br/>
         </div>
-      ) : (
-        <p>Loading...</p>
-      )}
-    </div>
-  );
-};
+    );
+}
 
 export default SsgPage;
